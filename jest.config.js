08a36b9bbdf51c1e35a7a29e4f6f63a6e663b123
yattom <tsutomu.yasui@gmail.com>
2025-11-20 20:53:34 +0900

    fix: test config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/out/'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
     '^.+\\.ts$': 'ts-jest'
